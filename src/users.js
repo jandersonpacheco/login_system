1620dd5b281c7dfb204f732199f17c74f0285a0e
Janderson Oliveira <janderson.katsu@gmail.com>
2024-07-23 11:16:49 -0300

    realizada a criação da tabela,  inclusão de linhas para contagem de usuários e botão de exclusão de usuário já funcional.

--- src/users.js
+++ src/users.js
@@ -14,7 +14,7 @@ document.getElementById('new-user-btn').addEventListener('click', () =>{
     const usernameLabel = AddLabel('user' + index, 'Username: ')
     const usernameInput = AddInput('user' + index, 'email')
     const passwordLabel = AddLabel('user' + index, 'Password: ')
-    const passwordInput = AddInput('user1' + index, 'password')
+    const passwordInput = AddInput('user' + index, 'password')
     const skipLabel = document.createElement('br')
 
     const registerBtn = document.createElement('button')
@@ -25,20 +25,43 @@ document.getElementById('new-user-btn').addEventListener('click', () =>{
     userData.append(usernameLabel, usernameInput, passwordLabel, passwordInput, skipLabel, registerBtn)
 
     registerBtn.addEventListener('click', ()=>{
-        const userData = document.getElementById('users')
         const username = usernameInput.value
         const password = passwordInput.value
 
         let user = {username: username, password: password}
             if(user.username !== '' && user.password !== ''){
                 users.push(user)
-            
+
                 console.log(users)
             } else{
                 alert('Preencha todos os campos!')
             }
     })
 }, {once: true})
+
+document.getElementById('show-table').addEventListener('click', function createTable() {
+    const table = document.getElementById('user-table-body-row')
+    table.innerText = '' 
+
+    users.forEach((data, i) =>{
+        const index = ++i
+        const tableRow = document.createElement('tr')
+        const position = addTableData(index, 'td-' + i)
+        const username = addTableData(data.username,  'td-' + i)
+        const password = addTableData(data.password, 'td-' + i)
+        const removeUser = document.createElement('button')
+        removeUser.innerText = 'Excluir usuário'
+
+        removeUser.addEventListener('click', () => {
+            users.splice(--i, 1)
+
+            createTable()
+        })
+
+        table.appendChild(tableRow)
+        tableRow.append(position, username, password, removeUser)    
+    })
+})
 
 function AddInput(id, text){
     const input = document.createElement('input')
@@ -57,9 +80,9 @@ function AddLabel(id, text){
     return label
 }
 
-function AddTableData(text, id){
+function addTableData(text, id){
     const tableData = document.createElement('td')
-    tableData.innerText = 
+    tableData.innerText = text
     tableData.id = id
 
     return tableData
