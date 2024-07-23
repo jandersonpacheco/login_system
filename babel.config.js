b1292e768ac7862f4735e70962f95363e4a6c0f9
Janderson Oliveira <janderson.katsu@gmail.com>
2024-07-23 14:04:44 -0300

    Realizado o empacotamento dos arquivos com webpack e criada a versão minimizada para rodar em browsers que não aceitem as funcionalidades modernas.

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: [
+        ['@babel/presets-env']
+    ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin')
+const path = require ('path')
+module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve (__dirname, 'dist')
+        }
+    },
+    entry: {
+        login_script: './src/script.js',
+        users_management_script: './src/users_management.js',
+        user_data_script: './src/users.js'
+    },
+    mode: 'production',
+    output: {
+        path: path.resolve (__dirname, 'dist'),
+        filename: '[name].min.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+}
